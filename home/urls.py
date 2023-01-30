8afcb542eff58fe717725074566de15c674912e5
Joe-Collins-1986 <collins.jw@btinternet.com>
2023-01-30 14:37:47 +0000

    add superuser and basic user registration with message

--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import (
     TravelHome,
+    register,
 )
 
 urlpatterns = [
     path('', TravelHome.as_view(), name="travel-home"),
+    path('register/', register, name='travel-register'),
 
 ]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserRegisterForm
 from django.views.generic import (
     View,
 )
@@ -7,6 +8,7 @@ from django.views.generic import (
 
 # Create your views here.
 class TravelHome(View):
+
     def get(self, request):
             
         return render(
@@ -14,6 +16,26 @@ class TravelHome(View):
             "home/travel-home.html",
             {
                 "tab_title": "Home"
-
             },
         )
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f"Your account has been created! You are now able to log in under username: {username}!")
+            return redirect('travel-home')
+    else:
+        form = UserRegisterForm()
+
+    return render(
+                request,
+                'home/register.html',
+                {
+                    "tab_title": "Register",
+                    "form": form
+                }
+            )
