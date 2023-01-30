dd112308329a7f268fd7cd3c8875e79f94ece595
Joe-Collins-1986 <collins.jw@btinternet.com>
2023-01-30 18:18:52 +0000

    change admin updates on reg page to absolute positoning

--- travel_app/settings.py
+++ travel_app/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'crispy_bootstrap5',
     'storages',
     'diary',
     'home',
@@ -137,6 +139,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Set crispy forms to boostrap 5
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 
 # AWS access keys and settings
