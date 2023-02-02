9bf3819aaad91abfcd79c34257c37d9ead546ea1
Joe-Collins-1986 <collins.jw@btinternet.com>
2023-02-02 11:23:15 +0000

    add password-reset routes and set up gmail host

--- home/urls.py
+++ home/urls.py
@@ -15,4 +15,26 @@ urlpatterns = [
          ),
          name='home-login'),
     path('logout/', auth_views.LogoutView.as_view(), name='home-logout'),
+
+
+    path('password-reset/',
+         auth_views.PasswordResetView.as_view(
+            template_name='home/password_reset.html'),
+         name='password_reset'),
+    
+
+    path('password-reset-confirm/<uidb64>/<token>',
+         auth_views.PasswordResetConfirmView.as_view(
+            template_name='home/password_reset_confirm.html'),
+         name='password_reset_confirm'),
+    
+    path('password-reset/done/',
+         auth_views.PasswordResetDoneView.as_view(
+            template_name='home/password_reset_done.html'),
+         name='password_reset_done'),
+
+    path('password-reset/complete',
+         auth_views.PasswordResetCompleteView.as_view(
+            template_name='home/password_reset_complete.html'),
+         name='password_reset_complete'),
 ]
--- travel_app/settings.py
+++ travel_app/settings.py
@@ -171,3 +171,12 @@ AWS_DEFAULT_ACL = None
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
+
+
+# Email set up for password reset
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
