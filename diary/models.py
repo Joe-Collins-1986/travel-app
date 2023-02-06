edf71ec179d0298f8e1f78658804a490c2e195a8
Joe-Collins-1986 <collins.jw@btinternet.com>
2023-02-06 11:33:25 +0000

    add country model

--- diary/models.py
+++ diary/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from PIL import Image
 
-# Create your models here.
+
+class Country(models.Model):
+    name = models.CharField(max_length=100, unique=True, default=None)
+    country_image = models.ImageField(default=None, upload_to='media/country_pics/', blank=True)
+    country_map = models.ImageField(default=None, upload_to='media/country_maps/', blank=True)
+    code = models.CharField(max_length=5, unique=True, default=None)
+    capital = models.CharField(max_length=100, unique=True, default=None)
+    region = models.CharField(max_length=100, default=None)
+    currency = models.CharField(max_length=100, default=None)
+    language = models.CharField(max_length=100, default=None)
+    about = models.CharField(max_length=500, default=None)
+    population = models.CharField(max_length=100, default=None)
+
+    def __str__(self):
+        return self.name
