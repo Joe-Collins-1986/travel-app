a639cf3c13fcc6bcc48153b599f126950e6a4662
Joe-Collins-1986 <collins.jw@btinternet.com>
2023-02-06 12:17:12 +0000

    add visitied model

--- diary/models.py
+++ diary/models.py
@@ -16,3 +16,15 @@ class Country(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Visit(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE,
+                             related_name="users_map")
+    country = models.ForeignKey(Country, on_delete=models.CASCADE,
+                                related_name="country_map")
+    status = models.CharField(max_length=50, default="not_visited")
+
+    def __str__(self):
+        return self.status
+
