61f2baadf61d705913e5d5108d5a397941580964
Joe-Collins-1986 <collins.jw@btinternet.com>
2023-02-10 10:46:01 +0000

    create setup view, url & html for update-detail

--- site_updates/urls.py
+++ site_updates/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 
 from .views import (
     AdminUpdatesListView,
+    AdminDetailUpdateView,
 )
 
 urlpatterns = [
     path('', AdminUpdatesListView.as_view(), name="all-admin-updates"),
+    path('<int:pk>/', AdminDetailUpdateView.as_view(), name="admin-update-detail"),
 ]
--- site_updates/views.py
+++ site_updates/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Update
 from django.views.generic import (
     ListView,
+    View,
 )
 
 
@@ -12,3 +13,18 @@ class AdminUpdatesListView(ListView):
     context_object_name = 'updates' # would pass object if not changed to post. Then would need to reference object not post in the template (see PostDetailView class for example)
     ordering = ["-published_on"]
     paginate_by = 5
+
+
+class AdminDetailUpdateView(View):
+
+    def get(self, request, pk):
+        update_objects = Update.objects.filter(status=1)
+        update = get_object_or_404(update_objects, pk=pk)
+
+        return render(
+            request,
+            "site_updates/admin-update-detail.html",
+            {
+                "update": update,
+            }
+        )
