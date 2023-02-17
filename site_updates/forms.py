0993fc2dc9e2fdad3e356b5379f1114ad33a4412
Joe-Collins-1986 <collins.jw@btinternet.com>
2023-02-17 16:31:17 +0000

    style comment on update detail page

--- site_updates/forms.py
+++ site_updates/forms.py
@@ -5,4 +5,4 @@ from django import forms
 class CommentForm(forms.ModelForm):
     class Meta:
         model = UpdateComment
-        fields = ('title', 'comment', 'comment_image')
+        fields = ['title', 'comment', 'comment_image']
