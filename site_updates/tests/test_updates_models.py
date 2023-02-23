0e05659110d5a689df3b9723184693b729afc075
Joe-Collins-1986 <collins.jw@btinternet.com>
2023-02-23 18:45:20 +0000

    add tests for update models

--- site_updates/tests/test_updates_models.py
+++ site_updates/tests/test_updates_models.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
-from site_updates.models import UpdateCatagory, Update
+from site_updates.models import UpdateCatagory, Update, UpdateComment
+from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class TestUpdateCatagoryModels(TestCase):
@@ -34,4 +36,40 @@ class TestUpdateModels(TestCase):
 
 
 
+class TestUpdateCommentModels(TestCase):
+
+    def setUp(self):
+        user1 = User.objects.create_user('JoeBloggs', 'JoeBloggs@test.com', 'Abc123456!')
+        self.catagory1 = UpdateCatagory.objects.create(
+            topic_catagory='catagory-1'
+        )
+
+        self.update1 = Update.objects.create(
+            topic=self.catagory1,
+            title='update-1',
+            content='content information'
+        )
+
+        self.comment1 = UpdateComment.objects.create(
+            site_update=self.update1,
+            author=user1,
+            title='test comment',
+            comment='test comment content'
+        )
+
+        self.client.login(username='JoeBloggs', password='Abc123456!')
+
+    def test_update_foreign_key_link(self):
+        self.assertEquals(self.comment1.site_update.title, 'update-1')
+    
+    def test_user_foreign_key_link(self):
+        self.assertEquals(self.comment1.author.username, 'JoeBloggs')
+
+    def test_model_str_returns_update_comment_title_attribute(self):
+        self.assertEquals(str(self.comment1), 'test comment')
+
+    def test_model_update_comment_absolute_url_name(self):
+        response = self.client.post(reverse('admin-update-detail',
+                                            args=[self.comment1.site_update.pk]))
+        self.assertEqual(response.status_code, 200)
 
