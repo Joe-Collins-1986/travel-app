6d2e32d83cd5936cb28ba788e01a5ee102a09175
Joe-Collins-1986 <collins.jw@btinternet.com>
2023-02-27 15:39:35 +0000

    add changable background to profile with opacity

--- user_profile/models.py
+++ user_profile/models.py
@@ -6,10 +6,20 @@ from django_resized import ResizedImageField
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     profile_img = ResizedImageField(
-                                    default='media/profiles/default-profile-img.jpeg',
-                                    upload_to='media/profiles/',
-                                    size=[300, None],
-                                    force_format='JPEG')
+                                default='media/profiles/default-profile-img.jpeg',
+                                upload_to='media/profiles/',
+                                size=[300, None],
+                                force_format='JPEG')
+
+    objectives = models.TextField(blank=True, null=True)
+
+    background_img = ResizedImageField(
+                                default='media/images/profile-bg-default.jpg',
+                                upload_to='media/profiles/background/',
+                                size=[1920, None],
+                                force_format='JPEG')
+
+
 
     def __str__(self):
         return f"{self.user.username} Profile"
