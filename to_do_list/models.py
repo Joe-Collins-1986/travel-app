b61cfe2ca4f61bb6a701d62fc99c7357d9bc8572
Joe-Collins-1986 <collins.jw@btinternet.com>
2023-03-08 11:00:43 +0000

    add todo list models

--- to_do_list/models.py
+++ to_do_list/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
+from diary.models import Country
 
 # Create your models here.
+class ToDoList(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    country = models.ForeignKey(Country, on_delete=models.CASCADE,
+                                related_name="country_to_do")
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    complete = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class ToDoItem(models.Model):
+    list = models.ForeignKey(ToDoList, on_delete=models.CASCADE)
+    item = models.CharField(max_length=200)
+    complete = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.item
+
+    class Meta:
+        ordering = ['complete']
