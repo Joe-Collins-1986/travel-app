1d3d81c7b5714f3cfa2393b8c03e2b56023c8706
Joe-Collins-1986 <collins.jw@btinternet.com>
2023-03-09 17:08:22 +0000

    unit test todo items view, delete, toggle complete

--- to_do_list/tests/test_to_do_views.py
+++ to_do_list/tests/test_to_do_views.py
@@ -107,4 +107,200 @@ class TestToDoListViews(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTrue(ToDoList.objects.filter(title='To Do List Title').exists())
 
+    def test_get_edit_to_do_lists(self):
+        self.client.login(username='JoeBloggs', password='Abc123456!')
+        url = reverse('edit-list', args=[self.to_do_list.pk])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
 
+    def test_get_edit_to_do_lists_not_logged_in(self):
+        url = reverse('edit-list', args=[self.to_do_list.pk])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 302)
+
+    def test_post_edit_to_do_lists(self):
+        self.client.login(username='JoeBloggs', password='Abc123456!')
+        url = reverse('edit-list', args=[self.to_do_list.pk])
+        data = {
+            'title': 'Updated To Do List Title', 
+        }
+        response = self.client.post(url, data)
+        self.assertRedirects(
+            response,
+            reverse('to-do-lists', args=[self.country1.pk]),
+            status_code=302,
+            target_status_code=200,
+            fetch_redirect_response=True)
+        self.assertTrue(ToDoList.objects.filter(title='Updated To Do List Title').exists())
+        self.assertFalse(ToDoList.objects.filter(title='To Do List Title').exists())
+
+    def test_post_edit_to_do_lists_invalid(self):
+        self.client.login(username='JoeBloggs', password='Abc123456!')
+        url = reverse('edit-list', args=[self.to_do_list.pk])
+        data = {
+            'title': '', 
+        }
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, 200)
+        self.assertFalse(ToDoList.objects.filter(title='').exists())
+        self.assertTrue(ToDoList.objects.filter(title='To Do List Title').exists())
+
+    def test_post_edit_to_do_lists_not_logged_in(self):
+        url = reverse('edit-list', args=[self.to_do_list.pk])
+        data = {
+            'title': 'Updated To Do List Title', 
+        }
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, 302)
+        self.assertFalse(ToDoList.objects.filter(title='Updated To Do List Title').exists())
+        self.assertTrue(ToDoList.objects.filter(title='To Do List Title').exists())
+
+    def test_post_edit_to_do_lists_wrong_user(self):
+        self.client.login(username='JaneBloggs', password='Xyz123456!')
+        url = reverse('edit-list', args=[self.to_do_list.pk])
+        data = {
+            'title': 'Updated To Do List Title', 
+        }
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, 200)
+        self.assertFalse(ToDoList.objects.filter(title='Updated To Do List Title').exists())
+        self.assertTrue(ToDoList.objects.filter(title='To Do List Title').exists())
+
+
+class TestToDoItemViews(TestCase):
+    
+    def setUp(self):
+        self.client = Client()
+        self.user1 = User.objects.create_user(
+            'JoeBloggs',
+            'JoeBloggs@test.com',
+            'Abc123456!')
+
+        self.user2 = User.objects.create_user(
+            'JaneBloggs',
+            'JaneBloggs@test.com',
+            'Xyz123456!')
+
+        self.country1 = Country.objects.create(
+            name='country-1',
+            code='AA',
+            capital='Capital',
+            region='Region',
+            currency='Pounds',
+            language='English',
+            about='Test content',
+            population='Sixy thousand',
+        )
+
+        self.to_do_list = ToDoList.objects.create(
+            user=self.user1,
+            country=self.country1,
+            title="To Do List Title",
+            description="To Do List Description",
+        )
+
+        self.to_do_item = ToDoItem.objects.create(
+            list=self.to_do_list,
+            item="To Do List Item",
+        )
+
+    def test_get_to_do_items(self):
+        self.client.login(username='JoeBloggs', password='Abc123456!')
+        url = reverse('to-do-items', args=[self.to_do_list.pk])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_to_do_items_not_logged_in(self):
+        url = reverse('to-do-items', args=[self.to_do_list.pk])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 302)
+
+    def test_post_to_do_items(self):
+        url = reverse('to-do-items', args=[self.to_do_list.pk])
+        self.client.login(username='JoeBloggs', password='Abc123456!')
+        data = {
+            'item': 'New To Do List Item', 
+        }
+        response = self.client.post(url, data)
+        self.assertRedirects(
+            response,
+            reverse('to-do-items', args=[self.to_do_item.pk]),
+            status_code=302,
+            target_status_code=200,
+            fetch_redirect_response=True)
+        self.assertTrue(ToDoItem.objects.filter(item='To Do List Item').exists())
+        self.assertTrue(ToDoItem.objects.filter(item='New To Do List Item').exists())
+
+    def test_post_to_do_items_not_logged_in(self):
+        url = reverse('to-do-items', args=[self.to_do_list.pk])
+        data = {
+            'item': 'New To Do List Item', 
+        }
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(ToDoItem.objects.filter(item='To Do List Item').exists())
+        self.assertFalse(ToDoItem.objects.filter(item='New To Do List Item').exists())
+
+    def test_post_to_do_items_invalid(self):
+        url = reverse('to-do-items', args=[self.to_do_list.pk])
+        self.client.login(username='JoeBloggs', password='Abc123456!')
+        data = {
+            'item': '', 
+        }
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(ToDoItem.objects.filter(item='To Do List Item').exists())
+        self.assertFalse(ToDoItem.objects.filter(item='').exists())
+
+    def test_delete_item(self):
+        self.client.login(username='JoeBloggs', password='Abc123456!')
+        url = reverse('delete-item', args=[self.to_do_item.pk])
+        response = self.client.get(url)
+        self.assertRedirects(
+            response,
+            reverse('to-do-items', args=[self.to_do_list.pk]),
+            status_code=302,
+            target_status_code=200,
+            fetch_redirect_response=True)
+        self.assertFalse(ToDoItem.objects.filter(item='To Do List Item').exists())
+
+    def test_delete_item_wrong_user(self):
+        self.client.login(username='JaneBloggs', password='Xyz123456!')
+        url = reverse('delete-item', args=[self.to_do_item.pk])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(ToDoItem.objects.filter(item='To Do List Item').exists())
+
+    def test_delete_item_not_logged_in(self):
+        url = reverse('delete-item', args=[self.to_do_item.pk])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(ToDoItem.objects.filter(item='To Do List Item').exists())
+
+    def test_toggle_complete_item(self):
+        self.client.login(username='JoeBloggs', password='Abc123456!')
+        url = reverse('complete-item', args=[self.to_do_item.pk])
+        response = self.client.get(url)
+        self.assertRedirects(
+            response,
+            reverse('to-do-items', args=[self.to_do_list.pk]),
+            status_code=302,
+            target_status_code=200,
+            fetch_redirect_response=True)
+        updated_item = ToDoItem.objects.get(pk=self.to_do_item.pk)
+        self.assertTrue(updated_item.complete)
+
+    def test_toggle_complete_item_not_logged_in(self):
+        url = reverse('complete-item', args=[self.to_do_item.pk])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 302)
+        updated_item = ToDoItem.objects.get(pk=self.to_do_item.pk)
+        self.assertFalse(updated_item.complete)
+
+    def test_toggle_complete_item_wrong_user(self):
+        self.client.login(username='JaneBloggs', password='Xyz123456!')
+        url = reverse('complete-item', args=[self.to_do_item.pk])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        updated_item = ToDoItem.objects.get(pk=self.to_do_item.pk)
+        self.assertFalse(updated_item.complete)
--- to_do_list/urls.py
+++ to_do_list/urls.py
@@ -17,6 +17,6 @@ urlpatterns = [
     path('to_do_lists/<int:pk>/edit', EditListView.as_view(), name="edit-list"), #pass ToDoList pk
 
     path('to_do_items/<int:pk>', ToDoItemsView.as_view(), name="to-do-items"), #pass ToDoList pk
-    path('complete_item/<int:pk>', CompleteItemView.as_view(), name="complete-item"), #pass ToDoList pk
-    path('delete_item/<int:pk>', DeleteItemView.as_view(), name="delete-item"), #pass ToDoList pk
+    path('complete_item/<int:pk>', CompleteItemView.as_view(), name="complete-item"), #pass ToDoItem pk
+    path('delete_item/<int:pk>', DeleteItemView.as_view(), name="delete-item"), #pass ToDoItem pk
 ]
