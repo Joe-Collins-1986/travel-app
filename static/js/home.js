80702d27c3710a90bc2d0725b16885bab41b891c
Joe-Collins-1986 <collins.jw@btinternet.com>
2023-03-21 10:50:49 +0000

    move home js into script

---
+++ static/js/home.js
@@ -0,0 +1,11 @@
+
+// This script is used to flash the arrow on home
+$('.blink').each(function() {
+var elem = $(this);
+elem.fadeOut(1000)
+    .fadeIn(1000)
+    .fadeOut(1000)
+    .fadeIn(1000)
+    .fadeOut(1000)
+    .fadeIn(1000)
+});
